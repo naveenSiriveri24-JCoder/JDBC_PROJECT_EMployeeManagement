ecb006068532e49d0a6535910fde6d38640a9e0e
naveenSiriveri24-JCoder <naveensiriveri.24@gmail.com>
2026-02-07 20:17:38 +0530

    Removed  database credentials

--- src/com/db_connection/Constants.java
+++ src/com/db_connection/Constants.java
@@ -2,7 +2,7 @@ package com.db_connection;
 
 public class Constants {
 
-	public static final String URL = "jdbc:mysql://localhost:3306/JDBC_PROJECT";
-	public static final String USERNAME = "root";
-	public static final String PASSWORD = "Naveen@sql24";
+	public static final String URL = "jdbc:mysql://localhost:3306/YOUR DATABASE NAME";
+	public static final String USERNAME = "YOUR USERNAME";
+	public static final String PASSWORD = "YOUR PASSWORD";
 }
